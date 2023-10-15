fe848b9f1093c890f89ffaa7291ad19f14a9e341
Awmteacck <147931086+Awmteacck@users.noreply.github.com>
2023-10-15 18:01:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801975492880","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801975492880","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801975492880"];
-global.OwnerName = "Slasher";
+global.Owner = ["918787651195"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["918787651195"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["918787651195"];
+global.OwnerName = "Awmtea Polythene";
 global.BotName = "BelugaBot-MD";
 global.packname = "Beluga-MD";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.author = "By: Awmtea";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/BelugaBot-MD"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/C4ivwZKuh5bLJkqfYNPQsk"; 
 
