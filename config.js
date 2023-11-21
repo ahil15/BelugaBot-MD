a0aeb82033b83b30e7d55d201d7129cf78727f78
7starrahul <116061452+7starrahul@users.noreply.github.com>
2023-11-21 13:26:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801975492880","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801975492880","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801975492880"];
-global.OwnerName = "Slasher";
-global.BotName = "BelugaBot-MD";
-global.packname = "Beluga-MD";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.Owner = ["918670116847"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["918670116847"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["918670116847"];
+global.OwnerName = "Rahul";
+global.BotName = "Rahul";
+global.packname = "Rahul";                             //Do not change.
+global.author = "By: Rahul";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/BelugaBot-MD"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/C4ivwZKuh5bLJkqfYNPQsk"; 
 
@@ -38,7 +38,7 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "Dhaka, Bangladesh";                   
+global.location = "Kolkata, India";                   
 global.reactmoji = "";
 global.themeemoji = "";
 global.vidmenu = { url: 'https://tenor.com/view/jujutsu-kaisen0-yuta-okkotsu-gif-26767662' };
